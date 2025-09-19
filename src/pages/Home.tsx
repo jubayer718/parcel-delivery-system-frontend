037172bbb9c08b052eb831c17e55e6dc398a6d6f
jubayer718 <jubayerahmed2462@gmail.com>
2025-09-19 07:13:00 +0600

    banner done

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,22 @@
 import Container from "@/components/ui/Container";
+import man1 from '@/assets/Images/man1.png'
 
 
 const Home = () => {
   return (
-    <Container>
-       <div>
-      This is Home page
-    </div>
+       <div className="bg-[url('@/assets/Images/bannerImg.jpg')] bg-center bg-cover lg:h-[500px] md:h-64 h-52 max-w-screen bg-no-repeat relative">
+    <Container className="flex items-center justify-evenly">
+    {/* Image */}
+        <div className="w-1/2">
+        <img src={man1} className="h-48 md:h-60 lg:h-[480px] -ml-8 lg:-ml-20 md:ml-8 " alt="" />
+        </div>
+        {/* content */}
+        <div className=" w-1/2">
+
+          <h1 className=" text-2xl md:text-4xl lg:text-5xl font-bold text-center">Delivering Your Parcels, On Time.<span className="text-orange-300"> Every Time.</span></h1>
+        </div>
    </Container>
+    </div>
   );
 };
 
