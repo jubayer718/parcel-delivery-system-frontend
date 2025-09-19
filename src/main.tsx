77bf1e42edb19746bedd1068da0605cafe528a17
jubayer718 <jubayerahmed2462@gmail.com>
2025-09-19 12:43:03 +0600

    Login, register and redux initialized

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,15 @@ import './index.css'
 
 import { RouterProvider } from 'react-router'
 import { router } from './routes/index.ts'
+import { Provider } from 'react-redux'
+import { store } from './Redux/store.ts'
+import { Toaster } from 'sonner'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Provider store={store}>
+      <Toaster richColors/>
     <RouterProvider router={router}/>
+    </Provider>
   </StrictMode>,
 )
